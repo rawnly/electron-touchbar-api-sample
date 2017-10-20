066eb2668351761a175b61fcac80d1990ec739b8
Federico Vitale <fedevitale99@gmail.com>
2017-10-21 00:13:28 +0200

    Core

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import {app, Menu, TouchBar, BrowserWindow} from 'electron';
+import path from 'path';
+
+import touchbar from './libs/touchbar';
+import menu from './libs/menu';
+
+
+app.on('ready', () => {
+	mainWindow(touchbar, menu);
+});
+
+function mainWindow(touchbar = false, menu = false) {
+	let thisWindow = new BrowserWindow({
+		width: 800,
+		height: 800,
+		resizable: true,
+		titleBarStyle: "hiddenInset"
+	});
+
+	thisWindow.loadURL('file://' + path.join(__dirname, 'render', 'index.html'));
+	
+	if (touchbar) 
+		thisWindow.setTouchBar(touchbar);
+
+	if (menu)
+		Menu.setApplicationMenu(menu);
+
+	return thisWindow;
+}
