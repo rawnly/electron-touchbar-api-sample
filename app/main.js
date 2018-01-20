d99dc6ac44601a0a3fb7c7b6b66bd4a34d86f00b
Federico Vitale <fedevitale99@gmail.com>
2018-01-20 21:19:57 +0100

    On Activate event re-create the window

--- app/main.js
+++ app/main.js
@@ -1,6 +1,9 @@
 // Require electron
 const electron = require('electron');
-const { BrowserWindow, app } = electron;
+const {
+	BrowserWindow,
+	app
+} = electron;
 
 // Require the touchbar from the `touchbar.js` file
 const touchbar = require('./touchbar');
@@ -8,8 +11,8 @@ const touchbar = require('./touchbar');
 // Let's define a new empty window
 let mainWindow;
 
-// Once app is ready
-app.on('ready', function() {
+// Ready function
+function ready() {
 
 	// "Fill" the window
 	mainWindow = new BrowserWindow({
@@ -23,15 +26,26 @@ app.on('ready', function() {
 	mainWindow.setTouchBar(touchbar);
 
 	// Load the render process
-	mainWindow.loadURL('file://' + __dirname + '/index.html')
-})
+	mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+	// On close destroy the window
+	mainWindow.on('close', () => {
+		mainWindow = null;
+	})
+};
+
+// Once app is ready
+app.on('ready', ready)
 
 // If all windows are all closed
 app.on('window-all-closed', () => {
-
 	// Kill the process if the platform is not "macOS / OSX"
 	if (process.platform !== 'darwin') {
 		app.quit();
 	}
+})
 
+app.on('activate', () => {
+	const windows = BrowserWindow.getAllWindows().length;
+	if (windows === 0) ready()
 })
